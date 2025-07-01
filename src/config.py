10f322b9aa0e2fac39b35e40ecb40e3c1886043c
Mahesh8214 <maheshsingh8214@gmail.com>
2025-07-01 21:09:38 +0530

    Initial commit

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@
 import os
 
 DEBUG = False
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "your-gemeni-api"
 LLM_MODEL = "llama-3.3-70b-versatile"
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 DATA_DIR = "./data"
